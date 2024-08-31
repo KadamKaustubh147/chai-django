8f2f7009a018ceeeb69646986113769e118b2642
Kaustubh <kaustubhkadam147@gmail.com>
2024-08-31 19:20:05 +0530

    Update

--- chaiaurdjango/chaiaurdjango/urls.py
+++ chaiaurdjango/chaiaurdjango/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact")
+    path("contact/", views.contact, name="contact"),
+    path("chai/", include("chai.urls")),
+    # ye settings.py se pata chalega chai app ke baare mei
+    # abhi ye include karke control transfer kardiya hai
+    # transfer from main app to this app control urls.py ka
 ]
